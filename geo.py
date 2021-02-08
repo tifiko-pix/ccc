b51292921804bbe11e3e27bec74361ab7d04fbe3
tifiko-pix <78507775+tifiko-pix@users.noreply.github.com>
2021-02-08 15:26:01 +0300

    Add files via upload

---
+++ geo.py
@@ -0,0 +1,25 @@
+
+
+import telebot
+from telebot import types
+
+token = "1614252826:AAE_cZSkij25y0vCR197edS82XpNSPenuIY"
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["geo"])
+def geo(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
+    keyboard.add(button_geo)
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и передай мне свое местоположение", reply_markup=keyboard)
+
+@bot.message_handler(content_types=["location"])
+def location(message):
+    if message.location is not None:
+        print(message.location)
+        print("latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude))
+
+
+
+bot.polling()
